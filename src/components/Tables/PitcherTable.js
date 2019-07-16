203aa15d8d689dd75f56ea25efc4dc065c1be38a
unknown <stanj89@gmail.com>
2019-07-15 20:24:48 -0400

    yogi

---
+++ src/components/Tables/PitcherTable.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core/'
+
+class PitcherTable extends React.Component {
+    render() {
+        return (
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Pitchers</TableCell>
+                            {this.props.pitchingCategories.map(category => {
+                                return <TableCell key={category.stat_id}>{category.display_name}</TableCell>
+                            })}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {this.props.pitchers.map(player => {
+                            return (
+                                <TableRow key={player.player_id}>
+                                    <TableCell>{player.name.full}</TableCell>
+                                    {player.player_stats.stats.stat.sort((a, b) => a.stat_id - b.stat_id)
+                                        .map(stat => {
+                                            return <TableCell key={stat.stat_id} align="right">{stat.value}</TableCell>
+                                        })}
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
+        )
+    }
+}
+
+export default PitcherTable
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,52 +1,33 @@
 import React from "react"
 import data from "./allMyData.json"
 //import Player from "./Player"
-import { Table, TableBody, TableCell, TableHead, TableRow, Paper } from '@material-ui/core/'
+import PitcherTable from "./Tables/PitcherTable.js";
+import HitterTable from "./Tables/HitterTable.js";
 
 class Team extends React.Component {
 
     state = {
-        myTeam: [],
-        statCategories: []
+        hitters: [],
+        pitchers: [],
+        hittingCategories: [],
+        pitchingCategories: []
     }
 
     componentDidMount() {
         this.setState({
-            myTeam: data.myPlayersStats.fantasy_content.players.player.filter(player => player.position_type === "B"),
-            statCategories: data.statIDs.stat.filter(stat => stat.position_types.position_type.includes("B"))
+            hitters: data.yogiStats.fantasy_content.players.player.filter(player => player.position_type === "B"),
+            pitchers: data.yogiStats.fantasy_content.players.player.filter(player => player.position_type === "P"),
+            hittingCategories: data.statIDs.stat.filter(stat => stat.position_types.position_type.includes("B")),
+            pitchingCategories: data.statIDs.stat.filter(stat => stat.position_types.position_type.includes("P"))
         })
     }
 
     render() {
-        console.log(this.state.myTeam)
-        console.log(this.state.statCategories)
-        console.log(this.state.statCategories.map(stat => stat.position_types))
         return (
-            <Paper>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Player Name</TableCell>
-                            {this.state.statCategories.map(category => {
-                                return <TableCell key={category.stat_id}>{category.display_name}</TableCell>
-                            })}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {this.state.myTeam.map(player => {
-                            return (
-                                <TableRow key={player.player_id}>
-                                    <TableCell>{player.name.full}</TableCell>
-                                    {player.player_stats.stats.stat.sort((a, b) => a.stat_id - b.stat_id)
-                                        .map(stat => {
-                                            return <TableCell key={stat.stat_id} align="right">{stat.value}</TableCell>
-                                        })}
-                                </TableRow>
-                            )
-                        })}
-                    </TableBody>
-                </Table>
-            </Paper>
+            <div>
+                <HitterTable hitters={this.state.hitters} hittingCategories={this.state.hittingCategories} />
+                <PitcherTable pitchers={this.state.pitchers} pitchingCategories={this.state.pitchingCategories} />
+            </div>
         )
     }
 }
